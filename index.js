ae7f353fea2eade5c0a6092f5d20fc7c43c38349
MariaAI <you@example.com>
2017-03-07 15:08:36 +0100

    letzter Stand

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-"use strict"
 /**
  * ToDo: Create dataStore module.
  *
@@ -8,7 +7,6 @@
 const collectionsConfig = require('./config/collectionsConfig')
     .config;
 let db;
-
 
 
 exports.init = (config) => {
@@ -28,106 +26,115 @@ exports.init = (config) => {
     return this
 }
 
-
-
 /**
+ * node-Objekt wird in der Datenbank in der Collection 'collection' gespeichert
  *
- * @param collection collection, in der User gespeichert sind, falls die collection noch nicht gibt, wird diese zunächst angelegt
- * @param node       neuer userObject, '_id' in dem Objekt ist optional, beim Fehlet der '_id' wird dieser automatisch erzeugt
- * @param callback   erstellter userObject
+ * @param collection {String} collection, in der User gespeichert sind, falls die collection noch nicht gibt, wird diese zunächst angelegt
+ * @param node {Object}       neuer userObject, '_id' in dem Objekt ist optional, beim Fehlet der '_id' wird dieser automatisch erzeugt
+ * @param callback {Function}  erstellter userObject
  */
-//todo Funktionen sind bisher nur Beispiel createUser muss ggf in das User Module etc.
-exports.createNode = (collection, node = {}, callback = ()=> {}) => {
+exports.createNode = (collection, node = {}, callback = ()=> {
+}) => {
     db.insertNode(collection, node, callback)
 }
 
 /**
+ * ein gerichtetes Edge wird angelegt und zwar gerichtet vom userIdFrom nach userIdTo
  *
- * @param collection collection, in der User gespeichert sind
- * @param userIdFrom Id des Users der dem User mit dem ID userIdTo folgt
- * @param userIdTo   Id des Users der von userIdFrom gefolgt wird
- * @param data       Eigenschaften der Beziehung
- * @param callback   UserObject mit dem Id userIdFrom
+ * @param collection {String}  collection, in der User gespeichert sind
+ * @param userIdFrom {String}  Id des Users der dem User mit dem ID userIdTo folgt
+ * @param userIdTo {String}    Id des Users der von userIdFrom gefolgt wird
+ * @param data {Object}     Eigenschaften der Beziehung
+ * @param callback {Function}   UserObject mit dem Id userIdFrom
  */
-exports.createConnection = (collection, userIdFrom, userIdTo, data = {}, callback = ()=> {}) => {
+exports.createConnection = (collection, userIdFrom, userIdTo, data = {}, callback = ()=> {
+}) => {
     db.insertConnection(collection, userIdFrom, userIdTo, data, callback)
 }
 
 /**
+ * data-Objekt wird in der Datenbank in der Collection 'content_collection' als Content gespeichert
+ * ein gerichtetes Edge wird vom userIdFrom aus der Collection 'user_collection' zu dem ContenObject angelegt
  *
- * @param content_collection    collection, in der Contents gespeichert sind
- * @param user_collection       collection, in der User gespeichert sind
- * @param userIdFrom           Id des Users der den ContentObject erstellt hat
- * @param data                 ContentObject
- * @param callback             erstellter ContentObject
+ * @param content_collection {String}    collection, in der Contents gespeichert sind
+ * @param user_collection {String}       collection, in der User gespeichert sind
+ * @param userIdFrom  {String}         Id des Users der den ContentObject erstellt hat
+ * @param data {Object}                ContentObject
+ * @param callback {Function}  erstellter ContentObject
  */
-exports.createContent = (content_collection, user_collection, userIdFrom, data = {}, callback = ()=> {}) => {
+exports.createContent = (content_collection, user_collection, userIdFrom, data = {}, callback = ()=> {
+}) => {
     db.insertContent(content_collection, user_collection, userIdFrom, data, callback)
 }
 
 /**
+ * Eigenschaften von UserObject('id') werden ergänzt oder ersetzt.
+ * Die drei Liste 'follow', 'followed' und 'content', sowie die Eigenschaft 'deleted'werden ignoriert.
+ * für den Content wir update nicht gebraucht, es werden nur neue Contents eingefügt
  *
- * @param collection collection, in der User gespeichert sind
- * @param id         id des Users
- * @param newData    existierende keys werden bei dem User überschrieben, neue hinzugefügt
- * @param callback   modifizierter User
- *für den Content wir update nicht gebraucht, es werden nur neue Contents eingefügt
+ * @param collection {String} collection, in der User gespeichert sind
+ * @param id {String}         id des Users
+ * @param newData {Object}    existierende keys werden bei dem User überschrieben, neue hinzugefügt
+ * @param callback {Function}   modifizierter User
+ *f
  */
-exports.updateNode = (collection, id, newData, callback = ()=> {}) => {
+exports.updateNode = (collection, id, newData, callback = ()=> {
+}) => {
     db.updateNode(collection, id, newData, callback)
 }
 
-//ToDo neo4j anpassen und diese f-n löschen
-exports.updateEdge = (relation, id, newData, callback = ()=> {}) => {
-    db.updateEdge(relation, id, newData, callback)
-}
-
 /**
+ * Die Eigenschaften des Edges die gerichtet von userIdFrom nach userIdTo vorliegt, werden ergänzt oder ersetzt.
  *
- * @param collection collection, in der User gespeichert sind
- * @param userIdFrom Id des Users der dem User mit dem ID userIdTo folgt
- * @param userIdTo   Id des Users der von userIdFrom gefolgt wird(weil wir zur Zeit die Eigenschaften doppelt speichern)
- * @param newData   neue Eigenschaften der Beziehung: die bestehenden Eigenschaften werden durch neue Eigenschaften komplett ersetzt
- * @param callback  UserObject mit dem Id userIdFrom
+ * @param collection {String} collection, in der User gespeichert sind
+ * @param userIdFrom {String} Id des Users der dem User mit dem ID userIdTo folgt
+ * @param userIdTo {String}   Id des Users der von userIdFrom gefolgt wird(weil wir zur Zeit die Eigenschaften doppelt speichern)
+ * @param newData {Object}   neue Eigenschaften der Beziehung: die bestehenden Eigenschaften werden durch neue Eigenschaften komplett ersetzt
+ * @param callback {Function} UserObject mit dem Id userIdFrom
  */
-exports.updateConnection = (collection, userIdFrom, userIdTo, newData, callback = ()=> {}) => {
+exports.updateConnection = (collection, userIdFrom, userIdTo, newData, callback = ()=> {
+}) => {
     db.updateConnection(collection, userIdFrom, userIdTo, newData, callback)
 }
 
 /**
+ * Eine Suchfunktion wird auf der Datenbank mit den Kriterien aus find-Object ausgelöst.
  *
- * @param collection collection, in der User bzw. Contents gespeichert sind
- * @param find       Kriterium, für den gesuchten User bzw. Content
- * @param callback   eine Liste der gefundenen Users bzw. Contents
+ * @param collection {String} collection, in der User bzw. Contents gespeichert sind
+ * @param find {Object}       Kriterium, für den gesuchten User bzw. Content
+ * @param callback {Function}  eine Liste der gefundenen Users bzw. Contents
  */
-exports.findNode = (collection, find, callback = ()=> {}) => {
+exports.findNode = (collection, find, callback = ()=> {
+}) => {
     db.findNode(collection, find, callback)
 }
 
 /*//die Liste aller Beziehungen werden mit dem User beim Suchen mitgeliefert, deswegen wird diese Funktion nicht gebraucht
-exports.findEdge = (relation, find, callback = ()=>{}) => {
-    db.findEdge(relation, find, callback)
-}
-*/
-/**
- *
- * @param collection collection, in der User bzw. Contents gespeichert sind
- * @param userId     Id des Users bzw. des Contents
- * @param callback
+ exports.findEdge = (relation, find, callback = ()=>{}) => {
+ db.findEdge(relation, find, callback)
+ }
  */
-exports.deleteNode = (collection, obId, callback = ()=> {}) => {
+/**
+ * Die Eigenschaft 'deleted' des Objectes('obId') wird auf 'true' gesetzt
+ *
+ * @param collection {String} collection, in der User bzw. Contents gespeichert sind
+ * @param userId {String}     Id des Users bzw. des Contents
+ * @param callback {Function} Obejct mit der Id obId
+ */
+exports.deleteNode = (collection, obId, callback = ()=> {
+}) => {
     db.deleteNode(collection, obId, callback)
 }
 
 /**
+ * Die Edges die gerichtet von userIdFrom nach userIdTo vorliegt wird aus der Datenbank entgültig gelöscht.
  *
- * @param collection  collection, in der User gespeichert sind
- * @param userIdFrom Id des Users der dem User mit dem ID userIdTo folgt
- * @param userIdTo   Id des Users der von userIdFrom gefolgt wird
- * @param callback   UserObject mit dem Id userIdTo
+ * @param collection {String}  collection, in der User gespeichert sind
+ * @param userIdFrom {String} Id des Users der dem User mit dem ID userIdTo folgt
+ * @param userIdTo {String}   Id des Users der von userIdFrom gefolgt wird
+ * @param callback {Function}  UserObject mit dem Id userIdTo
  */
-exports.deleteConnection = (collection, userIdFrom, userIdTo, callback = ()=> {}) => {
+exports.deleteConnection = (collection, userIdFrom, userIdTo, callback = ()=> {
+}) => {
     db.deleteConnection(collection, userIdFrom, userIdTo, callback)
-    //ToDo Neo4j noch anpassen
-    //db.deleteEdge(userId, callback)
 }
